6733899b11671d084e3622b30f12fe91b1b32e5f
Vlad Kondratiev <vlad.kondratiev@gmail.com>
2017-04-17 18:23:29 +0200

    updated correction factors for RS stations; updated info in make_casa_beamcorr.py

--- fluxcal/make_casa_beamcorr.py
+++ fluxcal/make_casa_beamcorr.py
@@ -6,28 +6,40 @@
 #
 # Vlad Kondratiev (c) - 05.08.2016
 #
+# 17.04.2017 - wrote info about the bug in antennaJones.py
+#              and updated casa_beamcorr_pkg.py for RS stations;
+#              also updated the stations' list
+#
 import os, sys, math
 import numpy as np
 from pyrap.measures import measures
 from pyrap.quanta import quantity
 import antennaJones as aj
 
-#stations=["CS001", "CS002", "CS003", "CS004", "CS005", "CS006", "CS007",
-#          "CS011", "CS013", "CS017", "CS021", "CS024", "CS026", "CS028",
-#          "CS030", "CS031", "CS032", "CS101", "CS103", "CS201", "CS301", 
-#          "CS302", "CS401", "CS501", "RS106", "RS205", "RS208", "RS210",
-#          "RS305", "RS306", "RS307", "RS310", "RS404", "RS406", "RS407",
-#          "RS409", "RS410", "RS503", "RS508", "RS509", "DE601", "DE602",
-#          "DE603", "DE604", "DE605", "FR606", "SE607", "UK608", "DE609",
-#          "PL610", "PL611", "PL612"]
-
-# currently tables are done only for CS and INTL stations. Mscorpol package
-# for whatever reasons fails to give Jones matrices for RS stations
+# In order to calculate correction factors for Remote stations (RS)
+# one needs to fix the bug in antennaJones.py script of mscorpol
+# package in line 210 (see below).
 stations=["CS001", "CS002", "CS003", "CS004", "CS005", "CS006", "CS007",
           "CS011", "CS013", "CS017", "CS021", "CS024", "CS026", "CS028",
           "CS030", "CS031", "CS032", "CS101", "CS103", "CS201", "CS301", 
-          "CS302", "CS401", "CS501", "DE601", "DE602", "DE603", "DE604", 
-          "DE605", "FR606", "SE607", "UK608", "DE609", "PL610", "PL611", "PL612"]
+          "CS302", "CS401", "CS501", "RS106", "RS205", "RS208", "RS210",
+          "RS305", "RS306", "RS307", "RS310", "RS406", "RS407", "RS409", 
+          "RS503", "RS508", "RS509", "RS511", "DE601", "DE602", "DE603",
+          "DE604", "DE605", "FR606", "SE607", "UK608", "DE609", "FI609", 
+          "PL610", "PL611", "PL612"]
+
+# currently tables are done only for CS and INTL stations. Mscorpol package
+# for whatever reasons fails to give Jones matrices for RS stations
+# There is a bug in mscorpol package script antennaJones.py the line 210. 
+# It has to be changed from "if stnLoc=='CS' or stnLoc=='RS':"  to  
+# "if stnLoc=='CS': # or stnLoc=='RS':", i.e. to comment out 'RS' part. 
+# It makes sense as all RS stations have just only one ear of 48 tiles similar 
+# to International stations with 96 tiles, thus antenna must be "HBA" instead of "HBA0"
+#stations=["CS001", "CS002", "CS003", "CS004", "CS005", "CS006", "CS007",
+#          "CS011", "CS013", "CS017", "CS021", "CS024", "CS026", "CS028",
+#          "CS030", "CS031", "CS032", "CS101", "CS103", "CS201", "CS301", 
+#          "CS302", "CS401", "CS501", "DE601", "DE602", "DE603", "DE604", 
+#          "DE605", "FR606", "SE607", "UK608", "DE609", "PL610", "PL611", "PL612"]
 
 #   m a i n
 if __name__=="__main__":
